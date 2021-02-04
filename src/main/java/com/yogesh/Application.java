3cc5df7f170e873c363ff82b3e43125743c73576
Yogesh Shandilya <yogeshshandilya01@gmail.com>
2021-02-04 14:05:50 +0530

    Modified Application file

--- src/main/java/com/yogesh/Application.java
+++ src/main/java/com/yogesh/Application.java
@@ -15,6 +15,8 @@ public class Application {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 	StudentRepo repo=context.getBean(StudentRepo.class);
+
+     System.out.println("Data JPA APP.....!");
 	
 	/*
 	 * Student s=new Student(); s.setStudentId(202);
